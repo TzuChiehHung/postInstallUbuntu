aa66711b056bc845d9fbd85d402c45ba669fe956
tc.hung <hungtc@solab.me.ntu.edu.tw>
2019-01-28 00:13:29 +0800

    Replace tmux-powerline with tmux

--- tmux-powerline.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/erikw/tmux-powerline.git ~/.tmux-powerline
-
-wget https://gist.githubusercontent.com/TzuChiehHung/1a9c42dd75583b1c5345b2ef215be674/raw/5da7e4d84fcc79a32b0493a2e8830525772da5e1/tmux.config -O ~/.tmux.conf
---
+++ tmux.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo apt-get install tmux
+
+cd
+git clone https://github.com/TzuChiehHung/.tmux.git
+ln -s -f .tmux/.tmux.conf
+
