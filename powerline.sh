b817920443072b2bebe4f7a5bf6a1225ef4b85ef
tc.hung <hungtc@solab.me.ntu.edu.tw>
2019-02-17 14:42:47 +0800

    Update powerline.sh

--- powerline.sh
+++ powerline.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install powerline
-
-mkdir -p ~/.fonts
-sudo wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -O ~/.fonts/PowerlineSymbols.otf 
-
-mkdir -p ~/.config/fontconfig/conf.d/
-sudo wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -O ~/.config/fontconfig/conf.d/10-powerline-symbols.conf 
-
-fc-cache -vf ~/.fonts/
+sudo apt-get -y install jq powerline
 
 echo "" >> ~/.bashrc
 echo "# Powerline" >> ~/.bashrc
@@ -17,6 +9,21 @@ echo "if [ -f \$POWERLINE_SCRIPT ]; then" >> ~/.bashrc
 echo "source \$POWERLINE_SCRIPT" >> ~/.bashrc
 echo "fi" >> ~/.bashrc
 
+# install fonts
+
+mkdir -p ~/.fonts
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -O ~/.fonts/PowerlineSymbols.otf
+mkdir -p ~/.config/fontconfig/conf.d/
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -O ~/.config/fontconfig/conf.d/10-powerline-symbols.conf
+
+fc-cache -vf ~/.fonts/
+
+# install fonts for vscode
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
 
 # show branch name
 POWERLINE_CONFIG=/usr/share/powerline/config_files/config.json
