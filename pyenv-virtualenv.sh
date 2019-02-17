25f94c1b349ed37ff522f5308d085e19fbb9429e
tc.hung <hungtc@solab.me.ntu.edu.tw>
2019-02-17 14:27:05 +0800

    Update pyenv-virtualenv.sh

--- pyenv-virtualenv.sh
+++ pyenv-virtualenv.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+sudo apt-get -y install \
+	curl\
+	libssl-dev\
+	libbz2-dev\
+	libreadline-dev\
+	libsqlite3-dev
 
+curl https://pyenv.run | bash
+
+echo '' >> ~/.bashrc
+echo '# pyenv' >> ~/.bashrc
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source ~/.bash_profile
 
-sudo apt-get install -y libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
